043cb45c80ef4cb0f939b28a46a23e933c64f45e
r-gunji <gunji@k-vanguard.co.jp>
2026-04-28 16:19:35 +0900

    ヘッダー・フッターのメニュー修正

--- resource/contact/confirm.php
+++ resource/contact/confirm.php
@@ -73,34 +73,31 @@
     <meta charset="utf-8">
     <meta name="description" content="きやま整体院のお問い合わせ内容の確認ページです。">
     <meta name="keywords" content="きやま整体院, お問い合わせ, お問い合わせ内容の確認, お問い合わせ内容の確認ページ">
-    <meta name="robots" content="index, follow">
+    <meta name="robots" content="noindex, nofollow">
     <meta content="IE=edge" http-equiv="X-UA-Compatible">
     <meta content="width=device-width, initial-scale=1" name="viewport">
     <meta content="telephone=no" name="format-detection">
     <link rel="canonical" href="https://kiyama-katakosiharikyuuin.com/">
-    <link rel="icon" href="https://tsurumiworks.com/assets/img/favicon.ico">
+    <link rel="icon" href="https://kiyama-katakosiharikyuuin.com/assets/img/favicon.ico">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@400;700&display=swap" rel="stylesheet">
     <link href="https://unpkg.com/ress@4.0.0/dist/ress.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://tsurumiworks.com/assets/css/style.css">
-    <link rel="stylesheet" href="https://tsurumiworks.com/assets/css/header.css">
-    <link rel="stylesheet" href="https://tsurumiworks.com/assets/css/footer.css">
-    <link rel="stylesheet" href="https://tsurumiworks.com/assets/css/contact.css">
+    <link rel="stylesheet" href="https://kiyama-katakosiharikyuuin.com/assets/css/style.css">
+    <link rel="stylesheet" href="https://kiyama-katakosiharikyuuin.com/assets/css/header.css">
+    <link rel="stylesheet" href="https://kiyama-katakosiharikyuuin.com/assets/css/footer.css">
+    <link rel="stylesheet" href="https://kiyama-katakosiharikyuuin.com/assets/css/contact.css">
   </head>
   <body class="page-contact">
 	<header class="site-header">
 		<div class="site-header__inner wrapper">
-			<a class="site-header__brand" href="https://tsurumiworks.com/index.html"><img alt="木山整体院 ロゴ" class="site-header__logo" src="https://tsurumiworks.com/assets/img/1776753879505.webp"> <span class="site-header__brand-text"><span class="site-header__name">きやま整体</span> <span class="site-header__subname">肩腰はりきゅう院</span></span></a> <button aria-controls="site-menu-panel" aria-expanded="false" aria-label="メニューを開く" class="site-header__menu-button js-menu-button" type="button"><span class="site-header__menu-line"></span> <span class="site-header__menu-line"></span> <span class="site-header__menu-line"></span></button>
+			<a class="site-header__brand" href="https://kiyama-katakosiharikyuuin.com/index.html"><img alt="木山整体院 ロゴ" class="site-header__logo" src="https://kiyama-katakosiharikyuuin.com/assets/img/1776753879505.webp"> <span class="site-header__brand-text"><span class="site-header__name">きやま整体</span> <span class="site-header__subname">肩腰はりきゅう院</span></span></a> <button aria-controls="site-menu-panel" aria-expanded="false" aria-label="メニューを開く" class="site-header__menu-button js-menu-button" type="button"><span class="site-header__menu-line"></span> <span class="site-header__menu-line"></span> <span class="site-header__menu-line"></span></button>
 		</div>
 	</header>
 	<div aria-hidden="true" class="site-menu-overlay js-menu-overlay"></div>
 	<nav aria-hidden="true" aria-label="グローバルメニュー" class="site-menu js-menu-panel" id="site-menu-panel">
 		<div class="site-menu__inner wrapper">
-			<ul class="site-menu__list">
-				<li class="site-menu__item">
-					<a aria-disabled="true" class="site-menu__link is-disabled" disabled>三つの特徴</a>
-				</li>
+      <ul class="site-menu__list">
 				<li class="site-menu__item">
 					<a aria-disabled="true" class="site-menu__link is-disabled" disabled>院長あいさつ</a>
 				</li>
@@ -108,16 +105,10 @@
 					<a aria-disabled="true" class="site-menu__link is-disabled" disabled>お客様の声</a>
 				</li>
 				<li class="site-menu__item">
-					<a aria-disabled="true" class="site-menu__link is-disabled" disabled>施術の流れ</a>
-				</li>
-				<li class="site-menu__item">
-					<a aria-disabled="true" class="site-menu__link is-disabled" disabled>料金</a>
-				</li>
-				<li class="site-menu__item">
 					<a aria-disabled="true" class="site-menu__link is-disabled" disabled>アクセス</a>
 				</li>
 				<li class="site-menu__item">
-					<a class="site-menu__link" href="https://tsurumiworks.com/contact/contact.php">お問い合わせ</a>
+					<a class="site-menu__link" href="https://kiyama-katakosiharikyuuin.com/contact/contact.php">お問い合わせ</a>
 				</li>
 			</ul>
 		</div>
@@ -168,8 +159,8 @@
 				<p class="site-menu-contact__text">ご予約フォーム・お電話・インスタDM・LINE等から、<br>
 				ご希望日時・メッセージをお送り下さい。</p>
 				<div class="site-menu-contact__buttons">
-					<a class="c-button c-button--line site-menu-contact__button--tel" href="tel:07035256934"><img alt="" class="c-button__icon" src="https://tsurumiworks.com/assets/img/tel-icon.svg"> 070-3525-6934</a> <a class="c-button c-button--line site-menu-contact__button--external" href="https://lin.ee/9mo0d6V" rel="noopener noreferrer" target="_blank">LINE <img alt="" class="c-button__icon" src="https://tsurumiworks.com/assets/img/external-link-icon.svg"></a>
-				</div><a class="c-button c-button--primary site-menu-contact__reserve-button" href="https://tsurumiworks.com/contact/contact.php">予約する</a>
+					<a class="c-button c-button--line site-menu-contact__button--tel" href="tel:07035256934"><img alt="" class="c-button__icon" src="https://kiyama-katakosiharikyuuin.com/assets/img/tel-icon.svg"> 070-3525-6934</a> <a class="c-button c-button--line site-menu-contact__button--external" href="https://lin.ee/9mo0d6V" rel="noopener noreferrer" target="_blank">LINE <img alt="" class="c-button__icon" src="https://kiyama-katakosiharikyuuin.com/assets/img/external-link-icon.svg"></a>
+				</div><a class="c-button c-button--primary site-menu-contact__reserve-button" href="https://kiyama-katakosiharikyuuin.com/contact/contact.php">予約する</a>
 			</div>
 		</section>
 	</nav>
@@ -312,14 +303,14 @@
 	<footer class="site-footer">
 		<div class="site-footer__main wrapper">
 			<div class="site-footer__info">
-				<a class="site-footer__brand" href="https://tsurumiworks.com/index.html"><img alt="木山整体院 ロゴ" class="site-footer__logo" src="https://tsurumiworks.com/assets/img/1776753879505.webp"> <span class="site-footer__brand-text"><span class="site-footer__name">きやま整体</span> <span class="site-footer__subname">肩腰はりきゅう院</span></span></a>
+				<a class="site-footer__brand" href="https://kiyama-katakosiharikyuuin.com/index.html"><img alt="木山整体院 ロゴ" class="site-footer__logo" src="https://kiyama-katakosiharikyuuin.com/assets/img/1776753879505.webp"> <span class="site-footer__brand-text"><span class="site-footer__name">きやま整体</span> <span class="site-footer__subname">肩腰はりきゅう院</span></span></a>
 				<div class="site-footer__address-row">
 					<address class="site-footer__address">
 						<span>〒769-1601</span> <span>香川県観音寺市豊浜町姫浜 701-1</span>
-					</address><a class="site-footer__detail-link" href="https://maps.app.goo.gl/cKzpzbnaSFheEMDWA" rel="noopener noreferrer" target="_blank">詳しくはこちら <img alt="" class="c-button__icon" src="https://tsurumiworks.com/assets/img/map-pin.svg"></a>
+					</address><a class="site-footer__detail-link" href="https://maps.app.goo.gl/cKzpzbnaSFheEMDWA" rel="noopener noreferrer" target="_blank">詳しくはこちら <img alt="" class="c-button__icon" src="https://kiyama-katakosiharikyuuin.com/assets/img/map-pin.svg"></a>
 				</div>
 				<div class="site-footer__cta">
-					<a class="c-button c-button--line site-footer__cta-button site-footer__cta-button--tel" href="tel:07035256934" rel="noopener noreferrer"><img alt="" class="c-button__icon" src="https://tsurumiworks.com/assets/img/tel-icon.svg"> <span>070-3525-6934</span></a> <a class="c-button c-button--line site-footer__cta-button" href="https://lin.ee/9mo0d6V" rel="noopener noreferrer" target="_blank"><span>LINE</span> <img alt="" class="c-button__icon" src="https://tsurumiworks.com/assets/img/external-link-icon.svg"></a> <a class="c-button c-button--line site-footer__cta-button" href="https://www.instagram.com/kiyama.katakosi.harikyuuinn?igsh=emtpMG4xdDg5YTV5&utm_source=qr" rel="noopener noreferrer" target="_blank"><span>Instagram</span> <img alt="" class="c-button__icon" src="https://tsurumiworks.com/assets/img/external-link-icon.svg"></a>
+					<a class="c-button c-button--line site-footer__cta-button site-footer__cta-button--tel" href="tel:07035256934" rel="noopener noreferrer"><img alt="" class="c-button__icon" src="https://kiyama-katakosiharikyuuin.com/assets/img/tel-icon.svg"> <span>070-3525-6934</span></a> <a class="c-button c-button--line site-footer__cta-button" href="https://lin.ee/9mo0d6V" rel="noopener noreferrer" target="_blank"><span>LINE</span> <img alt="" class="c-button__icon" src="https://kiyama-katakosiharikyuuin.com/assets/img/external-link-icon.svg"></a> <a class="c-button c-button--line site-footer__cta-button" href="https://www.instagram.com/kiyama.katakosi.harikyuuinn?igsh=emtpMG4xdDg5YTV5&utm_source=qr" rel="noopener noreferrer" target="_blank"><span>Instagram</span> <img alt="" class="c-button__icon" src="https://kiyama-katakosiharikyuuin.com/assets/img/external-link-icon.svg"></a>
 				</div>
 			</div>
 			<div class="site-footer__schedule">
@@ -357,34 +348,25 @@
 		<nav aria-label="フッターナビ" class="site-footer__nav">
 			<ul class="site-footer__nav-list wrapper">
 				<li>
-					<a aria-disabled="true" class="is-disabled" href="#" disabled>三つの特徴</a>
+					<a aria-disabled="true" class="is-disabled" href="#" disabled>院長あいさつ</a>
 				</li>
 				<li>
-					<a aria-disabled="true" class="is-disabled" href="#" disabled>今までの成り立ち</a>
-				</li>
-				<li>
-					<a aria-disabled="true" class="is-disabled" href="#" disabled>施術の流れ</a>
-				</li>
-				<li>
-					<a aria-disabled="true" class="is-disabled" href="#" disabled>営業時間</a>
-				</li>
-				<li>
-					<a aria-disabled="true" class="is-disabled" href="#" disabled>料金</a>
+					<a aria-disabled="true" class="is-disabled" href="#" disabled>お客様の声</a>
 				</li>
 				<li>
 					<a aria-disabled="true" class="is-disabled" href="#" disabled>アクセス</a>
 				</li>
 				<li>
-					<a href="https://tsurumiworks.com/contact/contact.php">お問い合わせ</a>
+					<a href="https://kiyama-katakosiharikyuuin.com/contact/contact.php">お問い合わせ</a>
 				</li>
 				<li>
-					<a href="https://tsurumiworks.com/privacy.html">プライバシーポリシー</a>
+					<a href="https://kiyama-katakosiharikyuuin.com/privacy.html">プライバシーポリシー</a>
 				</li>
 			</ul>
 		</nav>
 	</footer>
 
-    <script src="https://tsurumiworks.com/assets/js/style.js"></script>
+    <script src="https://kiyama-katakosiharikyuuin.com/assets/js/style.js"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
   </body>
 </html>
